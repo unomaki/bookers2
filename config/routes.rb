3f2df4fae9acdb65a670a39e897bb61ac745b2d9
EC2 Default User <ec2-user@ip-172-31-42-242.ap-northeast-1.compute.internal>
2023-02-15 13:04:27 +0000

    [Progressed]19-modelファイルにアソシエーションの記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit]
-  root to: "homes#top"
   devise_for :users
+  resources :users
+  resources :books
+  root to: "homes#top"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
