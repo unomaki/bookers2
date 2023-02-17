1040c934badec92329d6261461121aca6d5633af
EC2 Default User <ec2-user@ip-172-31-42-242.ap-northeast-1.compute.internal>
2023-02-17 22:42:59 +0000

    [Add]bookのdestroy機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,12 @@ class BooksController < ApplicationController
    @book = Book.find(params[:id])
   end
   
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    Book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+  end
+  
   private
 def book_params
 params.require(:book).permit(:title, :body)
