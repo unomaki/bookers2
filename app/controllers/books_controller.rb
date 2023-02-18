194bbbfff72b6c945a9dd1d4f9f2bfe5b96e4a54
EC2 Default User <ec2-user@ip-172-31-42-242.ap-northeast-1.compute.internal>
2023-02-18 23:46:16 +0000

    [Progressed]27-bookのindexページの左側の作成(User info)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,14 @@ class BooksController < ApplicationController
 
 
   def create
-    book = Book.new(book_id_params)
-    book.save
-    redirect_to '/top'
+
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path
+    else
+      render :new
+    end
   end
 
   def new
@@ -19,16 +24,16 @@ class BooksController < ApplicationController
   def edit
    @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     Book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
-  
+
   private
 def book_params
-params.require(:book).permit(:title, :body)
+params.require(:book).permit(:title, :body, :image)
 end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
          has_many :post_images, dependent: :destroy
+
+         validates :title, presence: true
+         validates :body, presence: true
+         validates :image, presence: true
 end
