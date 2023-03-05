c8b3b06de770b1c7d374344e5caf33e12180eade
EC2 Default User <ec2-user@ip-172-31-42-242.ap-northeast-1.compute.internal>
2023-03-05 12:29:12 +0000

    [Add]ログイン、新規登録、ログアウトのフラッシュメッセージの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
+
   def index
     @users = User.all
   end
 
 
   def show
-    @book = Book.find(params[:id])
     @user = User.find(params[:id])
+    @book = @user.books.all
   end
 
   def edit
