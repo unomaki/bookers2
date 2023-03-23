0c09ee76ea904e7f36c8c0d2e8f18b9bf180e253
EC2 Default User <ec2-user@ip-172-31-42-242.ap-northeast-1.compute.internal>
2023-03-23 14:45:54 +0000

    [Add]バリデーション新規投稿が失敗したとき

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,27 @@
 class BooksController < ApplicationController
 
 
-  def create
+  def new
+    @books=Book.all
+    @user=current_user
+  end
 
+
+  def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       flash[:notice]="You have created book successfully."
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books=Book.all
+      @user=current_user
+      render :index
     end
   end
 
-  def new
-  end
-
   def index
+    @book = Book.new
     @user=current_user
     @books=Book.all
   end
