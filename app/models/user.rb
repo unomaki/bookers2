039e8b0b44648f8dcffff157abfd754897c171ab
EC2 Default User <ec2-user@ip-172-31-42-242.ap-northeast-1.compute.internal>
2023-03-25 23:27:16 +0000

    [Update]aboutページのURLをhome/aboutに

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
          has_many :books, dependent: :destroy
 
         # validates :title, presence: true
-        validates :name, presence: true
+        validates :name, presence: true, length: {in: 2..20},uniqueness: true
+        validates :introduction, length: {maximum: 50}
 
          has_one_attached :profile_image
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :books
   root to: "homes#top"
-  get "homes/about"
+  get 'home/about' => "homes#about", as: 'about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
